30fec970a261bc6142483ac83f071e0bd0413cd7
killyEma <ekilly.23@gmail.com>
2019-01-01 23:22:56 -0300

    fix1

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const hbs = require('hbs');
-const fs = require('fs');
+//const fs = require('fs');
 
 const port = process.env.PORT || 3000;
 var app = express();
@@ -13,7 +13,7 @@ app.use((req, res, next) => {
   var log = `${now}: ${req.method} ${req.url}`;
 
   console.log(log);
-  fs.appendFile('server.log', log + '\n');
+  //fs.appendFile('server.log', log + '\n');
   next();
 });
 
